6b5f2e6113aa498e149bfbedd08be2d9d76ff1e4
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-02-13 18:12:03 -0600

    Create adashevKallistoRun.sh

---
+++ adashevKallistoRun.sh
@@ -0,0 +1,15 @@
+### Tj data
+#unsorted tj reads
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_tj/15 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_tj/SRR12476615.fastq.gz 
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_tj/16 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_tj/SRR12476616.fastq.gz
+#sorted tj reads
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_tj/17 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_tj/SRR12476617.fastq.gz
+time kallisto quant -i //home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_tj/18 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_tj/SRR12476618.fastq.gz
+
+### Eya data
+#unsorted eya
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_eya/19 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_eya/SRR12476619.fastq.gz
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_eya/20 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_input_eya/SRR12476620.fastq.gz
+#sorted eya
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_eya/21 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_eya/SRR12476621.fastq.gz
+time kallisto quant -i /home/madeline/research/Dm_index.idx -o /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_eya/22 -b 100 -t 4 --single -l 180 -s 20 /home/madeline/research/adashev_paper_analysis/adashev_raw_data/testes_sorting_eya/SRR12476622.fastq.gz
