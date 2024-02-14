3d29359f8d3f14600dfb9e2354b58c967d27a58b
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-02-13 18:07:18 -0600

    Create adashevDataDownlaod.sh
    Obtaining sequencing data from the paper by Adashev et al.

---
+++ adashevDataDownlaod.sh
@@ -0,0 +1,9 @@
+# Obtaining sequencing data from Adashev et al
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/015/SRR12476615/SRR12476615.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/016/SRR12476616/SRR12476616.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/017/SRR12476617/SRR12476617.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/018/SRR12476618/SRR12476618.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/019/SRR12476619/SRR12476619.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/020/SRR12476620/SRR12476620.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/021/SRR12476621/SRR12476621.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR124/022/SRR12476622/SRR12476622.fastq.gz
